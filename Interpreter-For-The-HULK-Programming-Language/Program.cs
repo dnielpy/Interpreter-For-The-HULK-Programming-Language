c1b67de641b97f6ebbe6c103b975beae7422e62e
dnielpy <dnielquesada@gmail.com>
2023-07-13 17:38:01 +0200

    new project using Dotnet v6.0

---
+++ Interpreter-For-The-HULK-Programming-Language/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Interpreter_For_The_HULK_Programming_Language
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
